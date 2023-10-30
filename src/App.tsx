5166411839c07ba311a33aa6602a3a88ff8e2ca6
Yaroslav Soromlya <yaroslav.soromlya@globallogic.com>
2023-10-30 09:08:26 +0200

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import styles from  './App.module.scss';
+import styles from './App.module.scss';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className={styles["App"]}>
-      <header className={styles["App-header"]}>
-        <img src={logo} className={styles["App-logo"]} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles["App-link"]}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Home />
+    </>
   );
 }
 
